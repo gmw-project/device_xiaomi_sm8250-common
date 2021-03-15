486322e448d1b3e258c3ad9ddb84de8f8cc253e8
goodmeow <harunbam3@gmail.com>
2021-03-15 15:28:23 +0700

    sm8250-common: sepolicy: address fingerprint sepolicy and remove unecessary guard for module

--- kona.mk
+++ kona.mk
@@ -207,12 +207,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-else ifeq ($(TARGET_NO_FOD),true)
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8250
 endif
+
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8250
 
 # GPS
 PRODUCT_COPY_FILES += \
